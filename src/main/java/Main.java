89658023cb1e32337760618f63d5db7b6f27d2c7
Ankit Sharma <ankit@intrepid.io>
2017-03-17 15:01:18 -0400

    Implemented BFS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 package main.java;
 
+import java.util.Scanner;
+
 /**
  * Created by ankit on 3/14/17.
  */
 public class Main {
     public static void main(String[] args) {
         NetworkGraph graph = LoadGraph.loadGraph();
-        graph.printGraph();
+        //graph.printGraph();
+        System.out.println("Enter two nodes to search");
+
+        Scanner scanner = new Scanner(System.in);
+        int source = scanner.nextInt();
+        int destination = scanner.nextInt();
+
+        boolean pathExists = graph.isNodeReachable(source, destination);
+
+        if (pathExists)
+            System.out.println("Path exists");
+        else {
+            System.out.println("No Path exists");
+        }
     }
 }
