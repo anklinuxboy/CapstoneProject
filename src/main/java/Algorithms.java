2e76c5c28bce1d40885064b969f5e7814e748810
Ankit Sharma <ankitsharma.nov11@gmail.com>
2017-03-26 12:50:59 -0400

    After rebase

---
+++ src/main/java/Algorithms.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import java.util.HashSet;
+
+/**
+ * Created by ankit on 3/26/17.
+ */
+public interface Algorithms {
+    // Return an unoptimized Dominating Set from the graph
+    public HashSet<Integer> naiveDominatingSet(Graph graph);
+
+    //
+}
